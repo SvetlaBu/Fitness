631b58bd7b56743a6a282406e07298f82357df0d
Sveta_Burakova <svetlaburs@gmail.com>
2023-07-15 20:37:12 +0300

    Настраивает десктоп аккордеона

--- source/js/main.js
+++ source/js/main.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   changeOffers();
-  sliderCoachers();
+  // sliderCoachers();
   initAccordions();
   // ---------------------------------
 
